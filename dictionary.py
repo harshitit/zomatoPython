3f92092d196d2d60fc23f17db7649b0b00784560
harshitit <harshitthakur1101@gmail.com>
2017-07-08 00:09:37 +0530

    requiredDictionary

---
+++ dictionary.py
@@ -0,0 +1,22 @@
+rest_list={
+    'Dominos Pizza':{
+    'Owner':'Dhruv',
+    'Rating':'4Star',
+    'Menu':{
+        '5Pepper':{'Price':'440'},
+        'Farmhouse':{'Price':'365'},
+        'Country Special':{'Price':'365'},
+        'Peppy Paneer':{'Price':'365'}
+    }
+    },
+    'Shree Ratnam':{
+    'Owner':'Harshit',
+    'Rating':'4.5Star',
+    'Menu':{
+        'Kadhai Paneer':{'Price':'300'},
+        'Paneer Korma':{'Price':'350'},
+        'Dal Makhni':{'Price':'275'},
+        'Roti':{'Price':'30'}
+    }
+    }
+}
